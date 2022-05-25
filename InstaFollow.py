63e9bb7a192a30216e73d8d725c38bf6a187122a
new92 <94779840+new92@users.noreply.github.com>
2022-05-25 10:23:21 +0300

    Update InstaFollow.py

--- InstaFollow.py
+++ InstaFollow.py
@@ -26,7 +26,11 @@ try:
     from geopy.geocoders import Nominatim
 except ImportError as imp:
     print("Error ! Please make sure you have installed all the modules !")
-    print("Modules used: instagrapi, nmap, time, sniffer, locale, pyfiglet, platform, socket, requests, os, sys, logging, geocoder, from geopy.geocoder import Nominatim")
+    time.sleep(1)
+    print("Please enter the command: pip3 install requirements.txt")
+    time.sleep(1)
+    print("And execute again the program")
+    time.sleep(1)
     print(imp)
 #End of Imports
 
@@ -42,12 +46,13 @@ print("[02] Exit")
 print("\n")
 option=input("[::] Choose an option: ")
 
-while option != "01" and option != "02":
+while option != "01" and option != "02" and option != "1" and option != "2":
     print("Invalid option !")
     option=input("[::] Please enter again: ")
-if option == "01":
+if option == "01" or option == "1":
     try:
         print("The data will not be stored or saved")
+        time.sleep(2)
         username=input("Please enter your username: ")
         time.sleep(1)
         password=input("Please enter your password: ")
@@ -56,68 +61,65 @@ if option == "01":
     except Exception as ex:
         print("Error !")
         print(ex)
-    clnt.login(username,password)
-    print("Please wait while the program is increasing your followers...")
-    print("To end the process enter Ctrl + C")
     try:
-        i = 1
-        while i > 0:
+        clnt.login(username,password)
+    except Exception as e:
+        print("Error !")
+        print(e)
+    print("Please wait while the program is increasing your followers...")
+    time.sleep(2)
+    print("This process might take some time...")
+    time.sleep(1)
+    print("To end the process enter Ctrl + C")
+    i = 1
+    while i > 0:
+        try:
             clnt.user_follow(173560420) #Cristiano Ronaldo
-            time.sleep(3)
             clnt.user_follow(1436859892) #Cardi B
-            time.sleep(3)
             clnt.user_follow(18428658) #Kim Kardashian
-            time.sleep(3)
             clnt.user_follow(7719696) #Ariana Grande
-            time.sleep(3)
             clnt.user_follow(451573056) #Nicki Minaj
-            time.sleep(3)
             clnt.user_follow(247944034) #Beyonce
-            time.sleep(3)
             clnt.user_follow(407964088) #Katy Perry
-            time.sleep(3)
             clnt.user_follow(460563723) #Selena Gomez
-            time.sleep(3)
             clnt.user_follow(6860189) #Justin Bieber
-            time.sleep(3)
             clnt.user_follow(427553890) #Lionel Messi
-            time.sleep(3)
             clnt.user_follow(26669533) #Neymar Jr
-            time.sleep(3)
             clnt.user_follow(4213518589) #Kylian Mbappe
-            time.sleep(3)
             clnt.user_follow(1234301500) #Karim Benzema
-            time.sleep(3)
+            clnt.user_follow(12331195) #Dua Lipa
+            clnt.user_follow(28527810) #Billie Eilish
+            clnt.user_follow(12281817) #Kylie Jenner
+            clnt.user_follow(208560325) #Khloe Kardashian
+            clnt.user_follow(145821237) #Kourtney Kardashian
+            clnt.user_follow(305701719) #Jennifer Lopez
+            clnt.user_follow(217867189) #Shakira
             clnt.user_unfollow(173560420) #Cristiano Ronaldo
-            time.sleep(3)
             clnt.user_unfollow(1436859892) #Cardi B
-            time.sleep(3)
             clnt.user_unfollow(18428658) #Kim Kardashian
-            time.sleep(3)
             clnt.user_unfollow(7719696) #Ariana Grande
-            time.sleep(3)
             clnt.user_unfollow(451573056) #Nicki Minaj
-            time.sleep(3)
             clnt.user_unfollow(247944034) #Beyonce
-            time.sleep(3)
             clnt.user_unfollow(407964088) #Katy Perry
-            time.sleep(3)
             clnt.user_unfollow(460563723) #Selena Gomez
-            time.sleep(3)
-            clnt.user_unfollow(6860189) #Justin Bieber 
-            time.sleep(3)
+            clnt.user_unfollow(6860189) #Justin Bieber
             clnt.user_unfollow(427553890) #Lionel Messi
-            time.sleep(3)
             clnt.user_unfollow(26669533) #Neymar Jr
-            time.sleep(3)
             clnt.user_unfollow(4213518589) #Kylian Mbappe
-            time.sleep(3)
             clnt.user_unfollow(1234301500) #Karim Benzema
-            time.sleep(3)
-    except KeyboardInterrupt as key:
-        print("Program interrupted !")
-        quit(0)
+            clnt.user_unfollow(12331195) #Dua Lipa
+            clnt.user_unfollow(28527810) #Billie Eilish
+            clnt.user_unfollow(12281817) #Kylie Jenner
+            clnt.user_unfollow(208560325) #Khloe Kardashian
+            clnt.user_unfollow(145821237) #Kourtney Kardashian
+            clnt.user_unfollow(305701719) #Jennifer Lopez
+            clnt.user_unfollow(217867189) #Shakira
+        except Exception as ex:
+            print("Error !")
+            print(ex)
+            quit(0)
 else:
     print("Exiting...")
     time.sleep(2)
     quit(0)
+#End of the program
