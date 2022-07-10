378a05dde311f911ba53ddb801ef63e3fb8a121b
new92 <94779840+new92@users.noreply.github.com>
2022-07-10 12:51:36 +0300

    Update InstaFollowV2.py

--- InstaFollowV2.py
+++ InstaFollowV2.py
@@ -34,9 +34,9 @@ try:
     from geopy.geocoders import Nominatim
     from instagrapi import *
 except ImportError as imp:
-    print("WARNING: Not all modules used in this program have been installed !")
+    print("[!] WARNING: Not all modules used in this program have been installed !")
     time.sleep(2)
-    print("Ignoring Warning...")
+    print("[+] Ignoring Warning...")
     time.sleep(2)
     system("sudo pip3 install -r requirements.txt")
 #End of Imports
@@ -54,44 +54,44 @@ print("\n")
 option=input("[::] Choose an option: ")
 
 while option != "01" and option != "02" and option != "1" and option != "2":
-    print("Invalid option !")
+    print("[!] Invalid option !")
     time.sleep(1)
     option=input("[::] Please enter again: ")
 if option == "01" or option == "1":
-    print("The data will not be stored or saved")
+    print("[+] The data will not be stored or saved")
     time.sleep(2)
-    username=input("Please enter your username: ")
-    while username == " " or username == "" or len(username) <= 0 or len(username) > 30:
-        print("Invalid Username !")
+    username=input("[+] Please enter your username: ")
+    while username == " " or username == "" or len(username) <= 2 or len(username) > 30:
+        print("[!] Invalid Username !")
         time.sleep(1)
-        username=input("Please enter again your username: ")
+        username=input("[+] Please enter again your username: ")
     username=username.lower()
     username=username.strip()
     time.sleep(1)
-    password=input("Please enter your password: ")
+    password=input("[+] Please enter your password: ")
     while password == " " or password == "" or len(password) <= 0:
-        print("Invalid Password !")
+        print("[!] Invalid Password !")
         time.sleep(1)
-        password=input("Please enter again your password: ")
+        password=input("[+] Please enter again your password: ")
     password=password.strip()
     time.sleep(1)
     clnt=instagrapi.Client()
     try:
         login = clnt.login(username,password)
         if login == True:
-            print("Login Successful !")
+            print("[!] Login Successful !")
             time.sleep(2)
-            print("Please wait while the program is increasing your followers...")
+            print("[+] Please wait while the program is increasing your followers...")
         else:
-            print("Login Unsuccessful !")
+            print("[!] Login Unsuccessful !")
             time.sleep(2)
-            print("Please check the username and/or the password !")
+            print("[+] Please check the username and/or the password !")
     except Exception as e:
-        print("Unexpected Error !")
+        print("[!] Unexpected Error !")
         time.sleep(1)
         print(e)
     time.sleep(2)
-    print("To end the process enter Ctrl + C")
+    print("[+] To end the process enter Ctrl + C")
     c = 1
     FOLLOW=['173560420','1436859892','18428658','7719696','451573056','247944034','407964088','460563723','6860189','427553890','26669533','4213518589','12331195','28527810','12281817','208560325','145821237','305701719','217867189','20824486','25025320','787132','260375673','290023231','1269788896','29394004','11830955','6380930','2094200507','9777455']
     UNFOLLOW=['173560420','1436859892','18428658','7719696','451573056','247944034','407964088','460563723','6860189','427553890','26669533','4213518589','12331195','28527810','12281817','208560325','145821237','305701719','217867189','20824486','25025320','787132','260375673','290023231','1269788896','29394004','11830955','6380930','2094200507','9777455']
@@ -100,10 +100,9 @@ if option == "01" or option == "1":
             try:
                 clnt.user_follow(FOLLOW[i])
             except Exception as key:
-                print("Unexpected Error !")
+                print("[!] Unexpected Error !")
                 time.sleep(4)
-                print("Exiting...")
-                time.sleep(1)
+                print("[+] Exiting...")
                 exit(0)
             print("[+] Following "+str(FOLLOW[i])+"...")
             time.sleep(2)
@@ -111,10 +110,9 @@ if option == "01" or option == "1":
             try:
                 clnt.user_unfollow(UNFOLLOW[e])
             except Exception as key:
-                print("Unexpected Error !")
+                print("[!] Unexpected Error !")
                 time.sleep(4)
-                print("Exiting...")
-                time.sleep(1)
+                print("[+] Exiting...")
                 exit(0)
             print("[+] Unfollowing "+str(UNFOLLOW[e])+"...")
             time.sleep(2)
